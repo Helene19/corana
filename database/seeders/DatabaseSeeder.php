4cca8433aab6a45ddfb8baca5cb4f7ffdc16c154
Helene19 <helene.scheibmair@gmx.at>
2021-04-10 20:16:39 +0200

    add empty seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $this->call(VaccinationPlaceTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(VaccinationTableSeeder::class);
     }
 }
